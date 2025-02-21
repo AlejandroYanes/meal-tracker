84608e4d4039adfa5fd10cbf3f4885d62b299c74
AlejandroYanes <alejandro.yanes94@gmail.com>
2025-02-21 23:29:14 +0000

    feat: implemented the mobile view for the nav-bar (v2)

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -19,7 +19,7 @@ export default function NavBar(props: Props) {
   }
 
   return (
-    <aside className="fixed inset-x-0 bottom-0 md:inset-y-0 md:left-0 z-10 w-full md:w-14 py-4 md:py-0 flex flex-row md:flex-col border-t md:border-r border-neutral-200 bg-white dark:bg-gray-950">
+    <aside className="fixed inset-x-0 bottom-0 md:inset-y-0 md:left-0 z-10 w-full md:w-14 py-2 md:py-0 flex flex-row md:flex-col border-t md:border-r border-neutral-200 bg-white dark:bg-gray-950">
       <nav className="flex md:flex-col justify-evenly md:justify-start items-center flex-1 gap-4 px-2 sm:py-5">
         <TooltipProvider>
           <Link
@@ -29,9 +29,9 @@ export default function NavBar(props: Props) {
             <SoupIcon />
             <span className="sr-only">Meal Tracker</span>
           </Link>
-          <NavItem href="/intake" icon={<CalendarDaysIcon className="h-5 w-5"/>} name="Intake records"/>
-          <NavItem href="/meals" icon={<CookingPotIcon className="h-5 w-5"/>} name="Meals"/>
-          <NavItem href="/groceries" icon={<ShoppingCartIcon className="h-5 w-5"/>} name="Groceries"/>
+          <NavItem href="/intake" icon={<CalendarDaysIcon className="h-7 md:h-5 w-7 md:w-5"/>} name="Intake records"/>
+          <NavItem href="/meals" icon={<CookingPotIcon className="h-7 md:h-5 w-7 md:w-5"/>} name="Meals"/>
+          <NavItem href="/groceries" icon={<ShoppingCartIcon className="h-7 md:h-5 w-7 md:w-5"/>} name="Groceries"/>
         </TooltipProvider>
       </nav>
     </aside>
@@ -46,7 +46,7 @@ function NavItem({ href, icon, name }: { href: string; icon: React.ReactNode; na
       <TooltipTrigger asChild>
         <Link
           data-active={pathname.startsWith(href)}
-          className="flex h-9 w-9 items-center justify-center text-gray-500 data-[active=true]:rounded-lg data-[active=true]:bg-gray-100 data-[active=true]:text-purple-900 transition-colors hover:text-gray-950 md:h-8 md:w-8 dark:bg-gray-800 dark:text-gray-50 dark:hover:text-gray-50"
+          className="flex h-10 w-10 md:h-8 md:w-8 items-center justify-center text-gray-500 data-[active=true]:rounded-lg data-[active=true]:bg-gray-100 data-[active=true]:text-purple-900 transition-colors hover:text-gray-950"
           href={href}
         >
           {icon}
