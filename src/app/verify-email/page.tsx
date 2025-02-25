c5d56fdb402eb532ea9b8eceebf45dc9cd419c1a
AlejandroYanes <alejandro.yanes94@gmail.com>
2025-02-25 22:43:36 +0000

    fix: updated verify page mobile view

--- src/app/verify-email/page.tsx
+++ src/app/verify-email/page.tsx
@@ -5,11 +5,11 @@ import { Button } from '@/ui';
 
 export default function VerifyEmailPage() {
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center">
-      <div className="mx-auto grid w-[480px] gap-6">
-        <div className="flex flex-col gap-2 text-center">
-          <h1 className="text-3xl font-bold">Welcome to Meal Tracker</h1>
-          <p className="text-center text-muted-foreground">
+    <div className="w-screen h-screen flex flex-col justify-center items-center px-4 md:px-0">
+      <div className="mx-auto grid md:w-[480px]">
+        <div className="flex flex-col gap-6 text-center">
+          <h1 className="text-2xl md:text-3xl font-bold">Welcome to Meal Tracker</h1>
+          <p className="text-center text-muted-foreground px-4 md:px-0">
             We have sent you an email to verify your account.
             <br/>
             Please check your inbox.
